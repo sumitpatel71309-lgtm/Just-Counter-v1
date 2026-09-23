046430ed9c85ffc83c787955e2863639c668eac6
SUMIT KUMAR PATEL <sumitpatel71309@gmail.com>
2026-09-24 00:24:52 +0530

    Refactor service worker registration and audio handling

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ if(window.location.pathname.includes("jap.html")){
 
 plus.addEventListener('pointerdown',()=>{
     if(navigator.vibrate){
-        navigator.vibrate(500);
+        navigator.vibrate(300);
     }
     m = m+1;
     n = n+1;
@@ -91,12 +91,12 @@ localStorage.setItem('total-t',o);
    minus.innerText = n;
    count.innerText = `108 X ${total}`;
    life.innerText = o;
-   triggerEffects(plus,500);
+   triggerEffects(plus,1000);
 })
 
 minus.addEventListener('pointerdown',()=>{
      if(navigator.vibrate){
-        navigator.vibrate(500);
+        navigator.vibrate(300);
     }
     if(o <= 0){
         return;
@@ -129,7 +129,7 @@ localStorage.setItem('total-t',o);
    minus.innerText = n;
    count.innerText = `108 X ${total}`;
    life.innerText = o;
-   triggerEffects(minus,500);
+   triggerEffects(minus,1000);
 });
 
 }
